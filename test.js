054398b4b0459ca397c77cb6909fde62c7741768
Snehil Patel <snehilp@eSummit-28-30th-Aug.local>
2021-07-07 20:27:00 +0530

    update the add function to take array

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-function add(a, b) {
-    return a + b
+function add(a) {
+    var sum = 0
+    for(let i = 0; i <a.length; i++) {
+        sum += a[i]
+    }
+    return sum
 }
 
-console.log(add(4, 5))
+console.log(add([4, 5, 6]))
